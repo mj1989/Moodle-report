03ee9e6bb26842b42e052ead0cf46985b0fb6c86
mj1989 <40517645+mj1989@users.noreply.github.com>
2023-08-27 22:12:01 +0200

    refactor: Separated data from view
    To make code cleaner and easy to read. Separated in file functions.php for both logic and HTML generation.

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 require_once('../../config.php'); // Include Moodle configuration
+require_once('functions.php'); // Include custom functions: generateTableUsersCourses
 
 // Check if the user is logged in and is an administrator
 require_login();
@@ -15,43 +16,11 @@ $PAGE->set_heading('MJ Report');
 // Display the page header
 echo $OUTPUT->header();
 
-// Fetch random users
+// Perform query - Fetch random users
 $random_users = $DB->get_records_sql('SELECT * FROM {user} ORDER BY RAND() LIMIT 10');
 
-// Display user information
-echo '<h2>Random Users</h2>
-        <table>
-            <tr>
-                <th>User</th>
-                <th>Courses</th>
-            </tr>';
-
-// Double loop to print random users and all courses for each user
-foreach ($random_users as $user) {
-    echo '<tr>
-            <td>' . fullname($user) . '</td>
-            <td>';
-    // Get array with all courses for specific user
-    $courses = enrol_get_users_courses($user->id);
-    // Lenght of the array with courses - number of courses for specific user
-    $lenght = count($courses);
-    // Index of first course
-    $index = 1;
-    foreach ($courses as $course) {
-        // If this is the last course it should end without comma
-        if ($index == $lenght) {
-            echo $course->fullname; 
-        }
-        // If the course is not the last then it should be separated by a comma for clarity of view in the table
-        else{
-            echo $course->fullname.', ';
-        }
-        $index = $index + 1;                
-    }
-    echo '</td>
-        </tr>';
-}
-echo '</table>';
+// Generate HTML - Display users and courses in a table
+generateTableUsersCourses($random_users);
 
 // Display the page footer
 echo $OUTPUT->footer();
